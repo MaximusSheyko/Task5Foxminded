ebb39e57472f5b0a2ff8d287953ec28b00ac55ba
Maxim Sheyko <maxim37377373@gmail.com>
2021-08-08 23:04:15 +0400

    add editconfig

---
+++ src/test/java/com/foxminded/TaskUniqueChar/UniqueCharCounterTest.java
@@ -0,0 +1,19 @@
+package com.foxminded.TaskUniqueChar;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class UniqueCharCounterTest {
+    
+
+    @Test
+    void testCountNumberOfUniqueChar() {
+	fail("Not yet implemented");
+    }
+
+}
